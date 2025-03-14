3d8f9a40bc4ad0819b78591b9313a20c8f03531e
pmsoo <pmsoo4@naver.com>
2025-03-14 16:35:28 +0900

    Initial

--- src/main/java/org/zerock/ex3/dto/SampleDTO.java
+++ src/main/java/org/zerock/ex3/dto/SampleDTO.java
@@ -8,12 +8,8 @@ import java.time.LocalDateTime;
 @Data
 @Builder(toBuilder = true)
 public class SampleDTO {
-
     private Long sno;
-
     private String first;
-
     private String last;
-
     private LocalDateTime regTime;
 }
